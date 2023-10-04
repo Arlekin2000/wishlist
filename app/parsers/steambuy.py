8d5defb336680a6b5564161628d489ecb0beda45
ivan <ivan4dante@gmail.com>
2023-10-04 18:07:10 +0500

    fix gaben_store

--- app/parsers/steambuy.py
+++ app/parsers/steambuy.py
@@ -1,18 +1,14 @@
 from bs4 import BeautifulSoup
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium import webdriver
 import time
 
 
 def steambuy(game):
-    #service = Service(executable_path=ChromeDriverManager().install())
-    #options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome("/home/ivan/workspace/wishlist/app/parsers/chromedriver")
+    driver = webdriver.Chrome()
 
     driver.get("https://steambuy.com/catalog/?q=" + "%20".join(game.lower().split()))
     time.sleep(1)
-    driver.execute_script("window.scrollTo(0, 2500)")
+    driver.execute_script("window.scrollTo(0, 5500)")
     time.sleep(1)
     soup = BeautifulSoup(driver.page_source, "lxml")
     name = [i.text for i in soup.findAll("div", class_="product-item__title")]
