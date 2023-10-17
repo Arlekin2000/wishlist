07c475673a0623788343e4cfa7e42103da42fb53
ivan <ivan4dante@gmail.com>
2023-10-17 16:45:15 +0500

    feat: add test

--- app/handlers/main.py
+++ app/handlers/main.py
@@ -6,7 +6,8 @@ from app.parsers.gaben_store import gaben_store
 
 @Client.on_message(filters.command("start", ["/"]))
 async def start(client, message: Message):
-    await client.send_message(message.from_user.id, "Hello world")
+    keyboard = [[InlineKeyboardButton(f"steampay")]]
+    await client.send_message(message.from_user.id, "Hello world", reply_markup=keyboard)
 
 
 @Client.on_message(filters.text)
@@ -15,9 +16,9 @@ async def get_game_name(client: Client, message: Message):
         game = f"Название: {i['name']}\n"
         keyboard = [[InlineKeyboardButton(f"steampay {i['price']}", url=i['link'])]]
         link = InlineKeyboardMarkup(keyboard)
-        await client.send_message(chat_id=message.from_user.id, text=game, reply_markup=link)
+        await client.send_message(message.from_user.id, game, reply_markup=link)
     for i in gaben_store(message.text):
         game = f"Название: {i['name']}\n"
         keyboard = [[InlineKeyboardButton(f"gabestore {i['price']}", url=i['link'])]]
         link = InlineKeyboardMarkup(keyboard)
-        await client.send_message(chat_id=message.from_user.id, text=game, reply_markup=link)
+        await client.send_message(message.from_user.id, game, reply_markup=link)
--- app/handlers/tests.py
+++ app/handlers/tests.py
@@ -1,7 +1,7 @@
 from pyrogram.types import Message
 from pyrogram.types.messages_and_media.message import Str
 
-from .main import start
+from .main import start, get_game_name
 
 
 async def test_start(bot, send_message, tg_user):
@@ -13,3 +13,25 @@ async def test_start(bot, send_message, tg_user):
     user_id, text = send_message.call_args.args
     assert user_id == tg_user.id
     assert text == "Hello world"
+
+async def test_get_game_name(bot, send_message, tg_user):
+    message = Message(text=Str("mafia"), id=1, from_user=tg_user)
+    async def response():
+        return True
+    send_message.side_effect = [
+        response(),
+        response(),
+        response(),
+        response(),
+        response(),
+        response(),
+        response(),
+        response()
+    ]
+    await get_game_name(bot, message)
+    assert send_message.called
+
+    assert send_message.call_count == 8
+    args = list(send_message.call_args_list)
+    breakpoint()
+    pass
